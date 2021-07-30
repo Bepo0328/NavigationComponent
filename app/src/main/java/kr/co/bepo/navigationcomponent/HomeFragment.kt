911c280670ef695420cd7b4475e7441e1c115912
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-07-30 22:07:31 +0900

    NavigationComponent: Navigation(Home -> Login)

--- app/src/main/java/kr/co/bepo/navigationcomponent/HomeFragment.kt
+++ app/src/main/java/kr/co/bepo/navigationcomponent/HomeFragment.kt
@@ -1,7 +1,35 @@
 package kr.co.bepo.navigationcomponent
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kr.co.bepo.navigationcomponent.databinding.FragmentHomeBinding
 
-class HomeFragment: Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = FragmentHomeBinding.inflate(inflater, container, false)
+        .also { binding = it }
+        .root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+    }
+
+    private fun initViews() = with(binding) {
+        buttonLogin.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
+    }
 }
---
+++ app/src/main/java/kr/co/bepo/navigationcomponent/LoginFragment.kt
@@ -0,0 +1,7 @@
+package kr.co.bepo.navigationcomponent
+
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+}
