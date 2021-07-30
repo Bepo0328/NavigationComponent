d86a3fcb3c5ca9f717cb86aa7b88b3c0c7b58d32
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2021-07-31 01:41:10 +0900

    NavigationComponent: Toolbar 생성

--- app/src/main/java/kr/co/bepo/navigationcomponent/MainActivity.kt
+++ app/src/main/java/kr/co/bepo/navigationcomponent/MainActivity.kt
@@ -2,10 +2,33 @@ package kr.co.bepo.navigationcomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import kr.co.bepo.navigationcomponent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initNavigation()
     }
+
+    private fun initNavigation() = with(binding) {
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean =
+        navController.navigateUp() || super.onSupportNavigateUp()
 }
